4e1d878f18709eda59ca44601bb2df13a1ea257d
MarcoBeraldo <beraldomar@gmail.com>
2022-06-29 14:32:18 +0200

    completed first exercise

--- js/palindromo.js
+++ js/palindromo.js
@@ -2,3 +2,39 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+
+// **********************************************************************
+
+// creo un prompt per chiedere una parola all' utente userWord
+
+// dichiaro la funzione isPalyndrome
+
+// ordino i caratteri della parola userWord in un senso orderedWord
+// ordino i caratteri nell'altro senso reversedWord
+// if (orderedWord === reversedWord){isPalyndrome ? : false}
+
+//come risultato ottengo il message ='la parola è palindroma' o 'la parola non è palindroma'
+
+// **********************************************************************
+
+const userWord = prompt('inserisci una parola');
+console.log(userWord);
+
+function isPalyndrome(userWord) {
+    let reversedWord = '';
+    let message = '';
+    for (let i = userWord.length - 1; i >= 0; i--) {
+        reversedWord += userWord[i];
+    }
+
+    if (reversedWord === userWord) {
+        message += 'la parola è palindroma';
+    } else message += 'la parola non è palindroma';
+
+
+    return message;
+
+}
+
+reversedWord = isPalyndrome(userWord);
+console.log(reversedWord)
